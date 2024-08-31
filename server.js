c24a208f83c6cbadbf5689b0ab1acdea5bd33537
Muhammad-mansur <muhammadmansur737@gmail.com>
2024-08-31 09:16:58 +0100

    changes

--- server.js
+++ server.js
@@ -1,14 +1,28 @@
 import express from "express";
 import bodyParser from "body-parser";
+import path from "path";
+import { fileURLToPath } from 'url';
+
+// Get the directory name of the current module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = 3000;
 
+// Set up EJS as the templating engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Body parser
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+// app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+    res.render("patient_dashboard.ejs", { userName: 'Mansur', userType: 'Doctor'});
 });
 
 app.listen(port, () => {
