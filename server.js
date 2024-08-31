e8cca7a0ab864dc80ce5ade59edf1393a7ab64ba
Muhammad-mansur <muhammadmansur737@gmail.com>
2024-08-31 08:18:02 +0100

    add

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+app.get("/", (req, res) => {
+    res.render("Hello world");
+});
+
 app.listen(port, () => {
     console.log(`server running on port ${port}`);
 });
