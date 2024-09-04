9f5f5037e87aa5e315638a5539d2c807e6f98673
Muhammad-mansur <muhammadmansur737@gmail.com>
2024-09-04 15:16:54 +0100

    update

--- routes/patient.js
+++ routes/patient.js
@@ -1,10 +1,16 @@
 import express from "express";
+import isAuthenticated from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-router.get("/patient_dashboard", (req, res) => {
-    res.render("patient_dashboard.ejs", {
-        userName: 'Muhammad Mansur', userType: 'patient'});
+router.get("/patient_dashboard", isAuthenticated, (req, res) => {
+    const user = req.session.user;
+
+    if (user && user.role === 'patient') {
+        res.render('patient_dashboard.ejs', { user: user });
+    } else {
+        res.redirect("/signin");
+    }
 });
 
 export default router;
